1a8855a979aab89db6b6c692867b5359bbe29a37
Antoine Aumjaud <antoine_dev@aumjaud.fr>
2017-05-14 22:46:49 +0200

    :sparkles: download dependency lib from github release

--- src/main/java/fr/aumjaud/antoine/services/docker/LaunchServer.java
+++ src/main/java/fr/aumjaud/antoine/services/docker/LaunchServer.java
@@ -1,48 +1,30 @@
 package fr.aumjaud.antoine.services.docker;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
 import static spark.Spark.post;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import static spark.Spark.port;
+import fr.aumjaud.antoine.services.common.PropertyHelper;
+import fr.aumjaud.antoine.services.common.TechnicalResource;
 
 public class LaunchServer {
 
-	private static Logger logger = LoggerFactory.getLogger(LaunchServer.class);
+	//private static Logger logger = LoggerFactory.getLogger(LaunchServer.class);
 
 	private static String COMMON_CONFIG_FILENAME = "common.properties";
 	private static String APP_CONFIG_FILENAME = "api-docker.properties";
 
 	public static void main(String... args) {
-		TechnicalResource commonResource = new TechnicalResource(loadProperties(COMMON_CONFIG_FILENAME));
-		DockerResource dockerResource = new DockerResource(loadProperties(APP_CONFIG_FILENAME));
+		PropertyHelper propertyHelper = new PropertyHelper();
+		TechnicalResource technicalResource = new TechnicalResource(propertyHelper.loadProperties(COMMON_CONFIG_FILENAME));
+		DockerResource dockerResource = new DockerResource(propertyHelper.loadProperties(APP_CONFIG_FILENAME));
 
 		port(9080);
 
-		get("/hi", commonResource::hi);
-		get("/info", commonResource::info);
+		get("/hi", (request, response) -> technicalResource.hi());
+		get("/info", (request, response) -> technicalResource.info());
 
 		post("/", "application/json", dockerResource::webhook);
 	}
 
-	private static Properties loadProperties(String configFileName) {
-		try (InputStream is = LaunchServer.class.getClassLoader().getResourceAsStream(configFileName)) {
-			if (is == null) {
-				throw new IllegalStateException("No config file in classpath: " + configFileName);
-			}
-			Properties p = new Properties();
-			p.load(is);
-			logger.info(configFileName + " loaded");
-			return p;
-		} catch (IOException e) {
-			logger.error("Can't load properties", e);
-			return null;
-		}
-	}
 }
